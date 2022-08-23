750bd49a29c03172c7a702a2bd7ad4fbe447a221
Antonio Zago <antoniozagodev@gmail.com>
2022-08-23 19:13:31 -0300

    Initial commit

---
+++ src/main/java/br/gov/sp/fatec/projectlabiv/ProjectlabivApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projectlabiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlabivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectlabivApplication.class, args);
+	}
+
+}
